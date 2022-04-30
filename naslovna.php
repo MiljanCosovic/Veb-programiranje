335119bf5f664b530ff8d6ebc6dbd06299197aee
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-04-30 13:27:07 +0200

    Project

--- naslovna.php
+++ naslovna.php
@@ -31,7 +31,7 @@
                         <li class="usluge"><a href="">Novosti</a></li>
                         <li class="simptomi"><a href="simptomi.php">Simptomi</a></li>
                         <li class="osoblje"><a href="">Osoblje</a></li>
-                        <li class="contact"><a href="">Kontak</a></li>
+                        <li class="contact"><a href="kontakt.php">Kontak</a></li>
                               
                         
                         <?php
